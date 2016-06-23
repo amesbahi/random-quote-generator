3610c0e67dee72448e9dc0be33bd5b78533ff4ef
Alborz Mesbahi <alborzmesbahi@WTA-MBP163.local>
2016-06-22 23:13:13 -0400

    Added quotes array

---
+++ js/quotes.js
@@ -0,0 +1,33 @@
+var quotes = [
+  {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }, {
+    quote: // text of the quote in a string to display on the page '',
+    source: // string identifying the creatore of the quote
+  }
+];
